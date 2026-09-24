cb37fe57c2c9a9066930e245ca48592a169b9919
DarvinInfantMichael <darvininfantmichael.26it@licet.ac.in>
2026-09-24 21:05:47 +0530

    updated

---
+++ Day20/frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+
+const Dashboard = () => {
+
+    const [products,setProducts] = useState([]);
+
+    
+
+  return (
+    <>
+    <div>
+
+        <h1>This is the DashBoard with Products Here</h1>
+
+
+    </div>
+    </>
+  )
+}
+
+export default Dashboard
