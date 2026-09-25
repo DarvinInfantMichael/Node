31088a3bdc7bf5c00cf510225ff2f509295450ea
DarvinInfantMichael <darvininfantmichael.26it@licet.ac.in>
2026-09-25 13:14:21 +0530

    updated

--- Day20/frontend/src/pages/Dashboard.jsx
+++ Day20/frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,40 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 const Dashboard = () => {
 
     const [products,setProducts] = useState([]);
+
+    const fetchFunction = async() =>{
+
+      try {
+
+        const fet = await fetch("https://dummyjson.com/products?limit=20");
+
+        const res = await fet.json();
+
+        setProducts(res.products);
+        
+      } catch (error) {
+
+        console.log("Error",error);
+        
+      }
+    }
+
+    useEffect (()=>{
+      fetchFunction();
+    })
+
+    const AllData = [...products];
+
+    const [search,setSearch]= useState("");
+
+    const HandleSearch=(e)=>{
+
+      setSearch(e.target.value);
+
+    }
 
     
 
@@ -12,6 +43,30 @@ const Dashboard = () => {
     <div>
 
         <h1>This is the DashBoard with Products Here</h1>
+
+        <h1>Avilable Products Are:</h1>
+
+        <label>Enter Product to Search :</label>
+        <input type="text"
+        onChange={HandleSearch}
+        value={search}
+        placeholder="Enter Name Of Product"/>
+
+        {AllData.map((e=>(
+
+          <div key={e.id}>
+
+          <img src={e.thumbnail}
+          alt={e.title}/>
+
+          <h2>{e.title}</h2>
+          <h2>{e.price}</h2>
+          <h2>{e.category}</h2>
+        </div>
+      
+        )))}
+
+        
 
 
     </div>
